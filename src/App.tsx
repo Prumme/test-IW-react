432961156810cca922e7c024a380360d9211e6ed
Prumme <aurelien23.p@gmail.com>
2024-06-19 10:46:05 +0200

    CI test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React here
+          Learn React here !!
         </a>
       </header>
     </div>
